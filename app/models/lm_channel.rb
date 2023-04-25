fc306769279c843538de14603c61bfa215b80230
nikolaokonesh <nikolaokonesh@gmail.com>
2023-04-26 09:22:50 +1100

    add

--- app/models/lm_channel.rb
+++ app/models/lm_channel.rb
@@ -6,4 +6,8 @@ class LmChannel < ApplicationRecord
   has_many :lm_channel_values
 
   broadcasts_to ->(lm_channel) { :lm_channels }
+
+  after_create_commit do
+    broadcast_append_to "lm_channels", target: "lm_channel_#{self.prefix}", partial: "lm_channels/lm_channel", locals: { lm_channel: self }
+  end
 end
