535462e823e4cb77c973685e528b4a8d130ec697
nikolaokonesh <nikolaokonesh@gmail.com>
2023-05-02 16:57:35 +1100

    reorg

--- app/models/lm_channel.rb
+++ app/models/lm_channel.rb
@@ -5,9 +5,11 @@ class LmChannel < ApplicationRecord
   validates :name, uniqueness: true
   has_many :lm_channel_values
 
-  broadcasts_to ->(lm_channel) { :lm_channels }
-
   after_create_commit do
     broadcast_append_to "lm_channels", target: "lm_channel_#{self.prefix}", partial: "lm_channels/lm_channel", locals: { lm_channel: self }
+  end
+
+  after_update_commit do
+    broadcast_update_to "lm_channels", target: "lm_channel_#{self.id}", partial: "lm_channels/lm_channel", locals: { lm_channel: self }
   end
 end
