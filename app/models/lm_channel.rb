f12f65ac00a029561dfea655dd384d00c4cdf404
nikolaokonesh <nikolaokonesh@gmail.com>
2023-05-08 11:48:59 +1100

    add last update channel

--- app/models/lm_channel.rb
+++ app/models/lm_channel.rb
@@ -12,4 +12,9 @@ class LmChannel < ApplicationRecord
   after_update_commit do
     broadcast_update_to "lm_channels", target: "lm_channel_#{self.id}", partial: "lm_channels/lm_channel", locals: { lm_channel: self }
   end
+
+  after_commit :last_update_channel, on: [ :update ]
+  def last_update_channel
+    broadcast_update_to('lm_channels', target: 'last_update_channel', partial: "lm_channels/last_update_channel", locals: { last_update_channel: LmChannel.order(:updated_at).last.updated_at })
+  end
 end
